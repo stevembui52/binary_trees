42b44497f4fd55b2315f92cceaec18683f1c03d8
Muigai-ben <muigaibenson37@gmail.com>
2023-11-28 03:35:17 -0800

    a function that deletes an entire binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - A function to delete a binary
+ *
+ * @tree: This points the node to be deleted
+ *
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
