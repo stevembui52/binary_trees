1d2f74270792f3c88fc07ba79d8bf6a1d92f7914
stevembui52 <stevembui52@gmail.com>
2023-11-30 04:17:06 -0800

    correct 15 full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -14,8 +14,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		return  (1);
 	if (tree->left && tree->right)
-	if (!tree->left && !tree->right);
-	   return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-}
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
+	return (0);
+}
