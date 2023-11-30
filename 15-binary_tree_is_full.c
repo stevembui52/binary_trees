69435ba1640f37486249ce56aa36c195c9b12484
Muigai-ben <muigaibenson37@gmail.com>
2023-11-30 01:48:25 -0800

    Write a function that checks if a binary tree is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,19 @@
+#include "binary_trees"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full.
+ *
+ * @tree -  is a pointer to the root node of the tree to check.
+ * Return - returns 0 if the function is NULL otherwise return 1.
+ */
+int binary_tree_is_full(const binary_tree_t *tree);
+{
+	if (tree == NULL)
+		Return 0;
+	if (tree->left == NULL && tree->right == NULL)
+		Return 0;
+	if ((tree->left) && (tree->right))
+		Return 1;
+}
---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,8 @@
+#include "binary_trees"
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is prfect
+ * @tree - is a pointer to the root node of the tree to check
+ *
